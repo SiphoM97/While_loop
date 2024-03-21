81696e2dfd0f592358fbd46e29210a54f36b83a3
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-03-21 03:22:01 +0200

    using while loop with lenght

--- While_loop.py
+++ While_loop.py
@@ -1,5 +1,20 @@
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
-    print("done with loop")
+#Example 1
+# i = 1
+# while i <= 10:
+#    print(i)
+#    i += 1
+#    print("done with loop")
+
+#Example 2:
+shoes = ["air max", "air force1", "jordan 3", "jordan 1", "yeezy desert boots", "fear of gods1"]
+shoe_i_want = "yeezy desert boots"
+
+length = len(shoes)
+count = 0
+while count < length:
+    print(shoes[count])
+
+    if shoes[count] == shoe_i_want:
+        print("placing order online for shoe")
+        break
+count += 1
